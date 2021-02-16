a4cc99b6e7e1fdf40fecd6f7940d1ec34ad64124
bcisaac <ibconner01@gmail.com>
2021-02-15 17:59:09 -0700

    Added Sample perspective API request
    updated requirements with google-python-api-client

--- app.py
+++ app.py
@@ -2,8 +2,14 @@ import os
 from slack_bolt import App
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
+from slacktastic.template import PieChart, Message
+from slacktastic.client import SlackClient
+from channel_history import on_home_opened
+from googleapiclient import discovery
 
-client = WebClient(token=os.getenv('token1'))
+client = WebClient(token=os.environ.get("SLACK_OAUTH_TOKEN"))
+slacktastic_client = SlackClient(webhook_url=os.environ.get("SLACK_WEBHOOK_URL"))
+API_KEY=AIzaSyAVBoD5zRdkarsx5f74HSgXVZLAx6EgOks
 
 app = App(
     token=(os.getenv('token1')),
@@ -28,3 +34,16 @@ def say_hi(say):
 #Don't run ./ngrok http 3000 again, it will deprecate the requests url in Slack and we will have to switch it
 if __name__ == "__main__":
     app.start(port=int(os.environ.get("PORT", 3000)))
+
+# Generates API client object dynamically based on service name and version.
+service = discovery.build('commentanalyzer', 'v1alpha1', developerKey=API_KEY)
+
+analyze_request = {
+  'comment': { 'text': 'friendly greetings from python' },
+  'requestedAttributes': {'TOXICITY': {}}
+}
+
+response = service.comments().analyze(body=analyze_request).execute()
+
+import json
+print( json.dumps(response, indent=2))
